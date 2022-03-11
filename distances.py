3beb1ce13261becedf932efe43df11bb958e46d7
Kong Chang <dev.kongchang@gmail.com>
2022-03-10 20:01:12 -0800

    Updated on package and working on distances now

---
+++ distances.py
@@ -0,0 +1,9 @@
+#Name: Kong Chang
+#Student ID: 010127362
+# C950 - Data Structures and Algorithms II
+
+import csv
+
+def load_data(filename):
+    with open(filename) as data:
+        
---
+++ truck.py
@@ -0,0 +1,7 @@
+#Name: Kong Chang
+#Student ID: 010127362
+# C950 - Data Structures and Algorithms II
+from package import package
+
+class truck:
+    def __init__(self):
