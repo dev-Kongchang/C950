5e69d46bb179a1b862ed4ee9a9ed5d30fce9703b
Kong Chang <dev.kongchang@gmail.com>
2022-03-19 15:25:36 -0700

    worked on distances

--- distances.py
+++ distances.py
@@ -2,13 +2,18 @@
 # Student ID: 010127362
 # C950 - Data Structures and Algorithms II
 
+from audioop import add
 import csv
+from traceback import print_tb
+from truck import truck
 
-# make variable 
 file_name = 'WGUPS Distance Table.csv'
+# make a global list so we can use later after uploading all the data from the csv
 distanceData = []
+# make a global dictionary to refer back to
+addressDictionary = {}
 # Summary: Make a 2D array (Adjacency Matrix)
-#          This will allow for a quick check for calculating distance between different places
+#          This will allow for a quick check for calculating distance between different places with using the dictionary as a guide
 
 # open the distance csv and attirbute variable to it
 with open(file_name) as data:
@@ -20,25 +25,45 @@ with open(file_name) as data:
     # Time Complexity O(N) since we are adding each row into the list
     for row in reader:
         distanceData.append(row)
-        
+
+# =================================================================================
+# the reason why we are making a dictionary now is so that later on when we are calculating the distance we won't need to look for the position again causing another for loop
+
+# Time complexity is O(N) for this loop
+for x in range(0, 28):
+    col_name = distanceData[0][x]
+    addressDictionary[distanceData[0][x]] = x
+
+
+# this function quickly utilizes the dictionary and gives us the distance 
+# by using the 2D array position
 def calculate_distance_between(address1, address2):
-    # make variable to hold address1 and address2
-    col = 0
-    row = 0
-    
-    # Since we are using an Adjacency Matrix, we only need to find the column and row position once
-    # therefore only needing one loop, Time Complexity O(N)
-    for x in range(0, 28):
-        col_name = distanceData[0][x]
+    # since we have the dictionary for the address, we can simply pull them up quickly rather than loop through the 2D array
 
-        if address1 == col_name:
-            col = x
-        if address2 == col_name:
-            row = x
-
-        if col != 0 and row != 0:
-            break
-    
+    # Time complexity is O(N)
+    col = addressDictionary.get(address1)
+    row = addressDictionary.get(address2)
     return distanceData[col][row]
 
-print(calculate_distance_between('2835 Main St', '3060 Lester St'))
+
+def minDistanceFrom(address1, truck):
+    # making variable placeholder so we don't modify the original and prevent bugs
+    truck_dummy = truck()
+    truck_dummy = truck
+    cargo = truck_dummy.get_cargo()
+    # making variable to start the first package to start comparing later on
+    min_distance = truck_dummy.get_first_cargo_distance()
+    # variable to hold the minimun distance address so we can return it at the end
+    min_address = ''
+
+    # Time Complexity is O(N) since we are calling calcuate_distance_between function which is only O(1)
+    for x in cargo:
+        distance = calculate_distance_between(address1, x)
+        if distance < min_distance:
+            min_distance = distance
+            min_address = x
+
+    return min_address
+
+
+#print(calculate_distance_between('2835 Main St', '3060 Lester St'))
--- truck.py
+++ truck.py
@@ -1,6 +1,7 @@
 #Name: Kong Chang
 #Student ID: 010127362
 # C950 - Data Structures and Algorithms II
+
 from package import package
 
 class truck:
@@ -10,10 +11,36 @@ class truck:
         self.cargo = []
         self.max_speed = 18
         
-    def add(self, package as package):
+
+    def add(self, package):
         if self.capacity == self.max_capacity:
-            
+            print('truck is full')
+            return False
         
         self.cargo.append(package)
         self.capacity += 1
+        return True
 
+    def get_cargo(self):
+        if self.cargo == []:
+            print('cargo is empty, please add some packages')
+            return False
+        else:
+            return self.cargo
+    
+    def get_first_cargo_distance(self):
+        if self.cargo == []:
+            print('cargo is empty, please add some packages')
+            return False
+        else:
+            return self.cargo[0]
+    
+    def get_current_package_address(self, index):
+        # make variable placeholder to get address
+        cargo = package()
+        cargo = self.cargo[index]
+        if cargo.get_delivery_city() == '':
+            print('delivery address is empty for package id: ' + str(cargo.get_id()))
+            return False
+        else:
+            return cargo.get_delivery_city()
