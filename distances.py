08d54981f6081409dd3b6166fed51d2ee759ec54
Kong Chang <dev.kongchang@gmail.com>
2022-03-18 20:05:52 -0700

    finished Adjacency Matrix programming

--- distances.py
+++ distances.py
@@ -3,22 +3,43 @@
 # C950 - Data Structures and Algorithms II
 
 import csv
+
+# make variable 
 file_name = 'WGUPS Distance Table.csv'
-
-# first we make the 2d array which hold our distance data
 distanceData = []
+# Summary: Make a 2D array (Adjacency Matrix)
+#          This will allow for a quick check for calculating distance between different places
 
+
+# open the distance csv and attirbute variable to it
 with open(file_name) as data:
+
+    rowData = []
+    # assign a variable to keep reading the data
     reader = csv.reader(data)
+
+    # Time Complexity O(N) since we are adding each row into the list
     for row in reader:
+        distanceData.append(row)
+        
+def calculate_distance_between(address1, address2):
+    # make variable to hold address1 and address2
+    col = 0
+    row = 0
+    
+    # Since we are using an Adjacency Matrix, we only need to find the column and row position once
+    # therefore only needing one loop, Time Complexity O(N)
+    for x in range(0, 28):
+        col_name = distanceData[0][x]
 
-        # make a list variable and loop through the total columns 
-        info = [] 
-        for i in range(0,29):
-            info.append(row[i])
+        if address1 == col_name:
+            col = x
+        if address2 == col_name:
+            row = x
 
-        # once the list if finished, we will append to the distanceData list
-        distanceData.append(info)
+        if col != 0 and row != 0:
+            break
+    
+    return distanceData[col][row]
 
-
-
+print(calculate_distance_between('2835 Main St', '3060 Lester St'))
