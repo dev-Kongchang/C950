a207197cc22188e77d430aea2c6e5b6ffc5f9c89
Kong Chang <dev.kongchang@gmail.com>
2022-03-23 09:57:09 -0700

    worked on Display module and putting in testing phrase for user input

--- display.py
+++ display.py
@@ -1,12 +1,14 @@
 # Name: Kong Chang
 # Student ID: 010127362
 # C950 - Data Structures and Algorithms II
-from abc import update_abstractmethods
+
+import string
 from package import Package
 from truck import Truck
 import deliverPackages
 from time import sleep
 import datetime
+from csv_reader import packageData
 
 # keep a global variable so we can update package #9 once
 updated = False
@@ -28,7 +30,74 @@ def display_delivery(giventruck, num):
         if update == False:
             if what.get_delivered_time() > datetime.timedelta(hours=10, minutes=20, seconds=0):
                 print('\n')
-                print('10:20:00: Package #9 was sucessfully updated with new address: 410 S State St., Salt Lake City, UT 84111 \n')
+                print(' 10:20:00: Package #9 was sucessfully updated with new address: 410 S State St., Salt Lake City, UT 84111 \n')
                 updated = True
-        print(str(what.get_delivered_time()) + ': Truck ' + str(num) +' delivered package #: ' + str(what.get_id()) + ' to address: ' + str(what.get_deliver_address()) + ', ' + str(what.get_deliver_city()) + ', ' + str(what.get_deliver_zip()))
+        print(' ' + str(what.get_delivered_time()) + ': Truck ' + str(num) +' delivered package #: ' + str(what.get_id()) + ' to address: ' + str(what.get_deliver_address()) + ', ' + str(what.get_deliver_city()) + ', ' + str(what.get_deliver_zip()))
         sleep(.1)
+
+# this function will contain our menu options for the user
+# we will keep this modular so we can come back to it if needed without
+# restarting the program, making us UI more user-friendly AND ANNOYING
+# Time Complexity = O(N)
+def menu():
+    print(' What do you want to view next?')
+    userinput = input(' 1.) Check a Package?\n 2.) Check Package Statuses between 8:35 am to 9:25 am? \n 3.) Check Package Statuses between 9:35 am to 10:25 am? \n 4.) Check Package Statuses between 12:03 pm to 1:12 pm?\n\n')
+    # error-checking for user input
+    convert = 0
+    try:
+        convert = int(userinput)
+    except ValueError:
+        print(' Please enter an valid option...')
+        menu()
+    return convert
+
+def check_package():
+    print('Please choose one of the following options for a Package/Packages:')
+    print(' 1.) View Package by ID?')
+    print(' 2.) View Package by delivery address?')
+    print(' 3.) View Package by delivery deadline?')
+    print(' 4.) View Package by delivery city?')
+    print(' 5.) View Package by delivery zip code?')
+    print(' 6.) View Package by weight?')
+    print(' 7.) View Package by status?')
+    print(' 8.) Go back to previous screen?')
+    userinput = input(' 9.) Quit? \n')
+
+    convert = 0
+    try:
+        convert = int(userinput)
+    except ValueError:
+        print(' Please enter an valid option...')
+        check_package()
+    
+    # we quit the program
+    if convert == 9:
+        print(' Thank you for your time, Have a great day!')
+        exit(0)
+
+    # go back to the menu section
+    if convert == 8:
+        sleep(.1)
+        menu()
+
+    # to check package by id, we will loop through the hash table
+    if convert == 1:
+        check_package_id()
+    
+        
+
+
+def check_package_id():
+    sleep(.1)
+
+    userinput = input(' Please Enter the Package ID: \n')
+
+    try:
+        int(userinput)
+    except ValueError:
+        print(' You did not enter an ID Number, Please try again...')
+    # always using placeholder, so we don't modify the original
+    what = packageData() 
+    package = Package() 
+    package = what.get()
+    if 
--- main.py
+++ main.py
@@ -10,63 +10,86 @@ from package import Package
 import display
 
 class Main:
-    print('Hello! Welcome to the C950 - Data Structures and Algorithms \n')
-    print('Performance Assessment - WGUPS Program\n')
+    print('     Hello! Welcome to the C950 - Data Structures and Algorithms \n')
+    print('     Performance Assessment - WGUPS Program\n')
 
-    userinput = int(input('What do you want to do?: \n' + '1 - Start Program excution?\n' + '2 - Quit?\n \n'))
+    userinput = int(input(' What do you want to do?: \n' + '1 - Start Program excution?\n' + '2 - Quit?\n \n'))
 
     if userinput == 1:
-        print('Starting Delivery simulation..... \n')
+        print(' Starting Delivery simulation..... \n')
         sleep(1)
         orderlist = deliverPackages.truck1.cargo
         what = Package()
 
-        print('Truck 1 Package Loading:')
+        print(' Truck 1 Package Loading:')
         # Time Complexity = O(N)
         for x in range(0, len(orderlist)):
             what = orderlist[x]
-            print('Package #' + str(what.get_id()) + ' is loaded into Truck 1')
+            print(' Package #' + str(what.get_id()) + ' is loaded into Truck 1')
             sleep(.1)
 
         
         orderlist = deliverPackages.truck2.cargo
         print('\n')
-        print('Truck 2 Package Loading:')
+        print(' Truck 2 Package Loading:')
         # Time Complexity = O(N)
         for x in range(0, len(orderlist)):
             what = orderlist[x]
-            print('Package #' + str(what.get_id()) + ' is loaded into Truck 2')
+            print(' Package #' + str(what.get_id()) + ' is loaded into Truck 2')
             sleep(.1)
 
         
         orderlist = deliverPackages.truck3.cargo
         print('\n')
-        print('Truck 3 Package Loading:')
+        print(' Truck 3 Package Loading:')
         # Time Complexity = O(N)
         for x in range(0, len(orderlist)):
             what = orderlist[x]
-            print('Package #' + str(what.get_id()) + ' is loaded into Truck 3')
+            print(' Package #' + str(what.get_id()) + ' is loaded into Truck 3')
             sleep(.1)
 
 
         # ====================== Delivery Execution ===========================
         print('\n')
-        print('Starting Delivery Execution')
+        print(' Starting Delivery Execution')
         print('\n')
 
-        print('Truck 1 Delivery:')
+        print(' Truck 1 Delivery:')
         # Time Complexity = O(N)
         display.display_delivery(deliverPackages.truck1, 1)
 
-        print(str(deliverPackages.truck1.get_current_time()) + ': Truck 1 went back to base hub to switch drivers with Truck 2')
+        print(' ' + str(deliverPackages.truck1.get_current_time()) + ': Truck 1 went back to base hub to switch drivers with Truck 2')
 
         print('\n')
-        print('Truck 3 Delivery')
+        print(' Truck 3 Delivery')
+        # Time Complexity = O(N)
         display.display_delivery(deliverPackages.truck3, 3)
 
         print('\n')
-        print('Truck 2 Delivery')
+        print(' Truck 2 Delivery')
+        # Time Complexity = O(N)
         display.display_delivery(deliverPackages.truck2, 2)
+
+        # now we display the total miles of each truck and all together
+        print('\n')
+        print(' Day Ended at: ' + str(deliverPackages.truck2.get_current_time()))
+        sleep(.1)
+        print(' Truck 1 Total Miles: ' + str(deliverPackages.truck1.get_miles()))
+        sleep(.1)
+        print(' Truck 2 Total Miles: ' + str(deliverPackages.truck2.get_miles()))
+        sleep(.1)
+        print(' Truck 3 Total Miles: ' + str(deliverPackages.truck3.get_miles()))
+        sleep(.1)
+
+        total = float(deliverPackages.truck1.get_miles()) + float(deliverPackages.truck2.get_miles()) + float(deliverPackages.truck3.get_miles())
+        print(' Total Miles for the whole system is: ' + str(total)+ '\n')
+        # 
+
+        userinput = display.menu()
+
+        if userinput == 1:
+            display.check_package()
+
 
     else:
         print('Thank you for your time and Have a Awesome Day!')
